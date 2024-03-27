579e7640aced52d7a546ef686d0df74cc8fb2b4b
Prajwal ps <162997646+praxjt@users.noreply.github.com>
2024-03-27 23:12:26 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,23 @@
+console.log("hungry shrak")
+let _kali =7;
+console.log(_kali)
+//let @ho=8;  sighn is not allowed
+//console.log(@ho)
+//var 5kol=8;
+//console.log(5kol)  number cannot store
+let $hai=8;//$can be used as start
+console.log($hai)
+let fulll$NAMe=7;
+console.log(fulll$NAMe)
+var age=45
+var age=19
+var age=17
+console.log(age)//variable can be re-declared
+let ag =20
+ag=28
+ag=18
+console.log(ag)//variable cannot be re-declared but can be modified
+const eg=2
+ //eg=19 cannot modified
+console.log(eg)//variable cannot  re-declared or modified
+
