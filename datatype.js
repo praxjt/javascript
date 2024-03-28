8086ca9a8e3bb681d1dfe84937b65468d4cc04d2
Prajwal ps <162997646+praxjt@users.noreply.github.com>
2024-03-28 21:52:42 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,26 @@
+
+//data types to check number
+
+
+//primitive data type
+let kio=24;
+typeof kio;// type in default browser prints 'number' typeof apply to below code as well
+
+let kai='ferari';// typeof default browser prints 'string'
+ 
+anixty=true;// typeof default browser prints 'boolean'
+
+let $;// typeof default browser prints 'undefined'
+
+let _x=null;// typeof default browser prints 'object'
+ 
+
+//non primitive 
+const jucie={
+    fruit:'apple',
+    price:20,
+    sugar:'_1tbs',
+}
+console.log(jucie.fruit)//to print values
+jucie['price']=jucie['price']+1//to change value
+// usually cons keyword variables cannot be modified but can change obejct of keys
